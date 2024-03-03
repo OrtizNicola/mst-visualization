cb5872cd85358a73f40adccd9e5c8def4fdd30e1
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-03-02 23:38:50 -0500

    add comments

--- sketch.js
+++ sketch.js
@@ -1,14 +1,23 @@
+// constants for the nodes
 let COLOR = 255;
-let COLOR_EDGE = 255;
-let WEIGHT = 5;
-let n_nodes = 5;
+let COLOR_CHOSEN = 120;
 let RADIUS = 10;
 
+// constants for the edges
+let COLOR_EDGE = 255;
+let WEIGHT = 5;
+
+// array of all the nodes of the graph
+let n_nodes = 5;
 let nodes = [];
+
+// the chosen node is the one the user wants to move around
 let chosen = null;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+
+    // initialize all of the nodes
     for (let i = 0; i < n_nodes; i++) {
         nodes[i] = new Node(random(width), random(height));
     }
@@ -17,6 +26,7 @@ function setup() {
 function draw() {
     background(0);
 
+    // show all of the nodes
     for (let i = 0; i < n_nodes; i++) {
         nodes[i].show();
     }
@@ -24,6 +34,7 @@ function draw() {
 
 function mousePressed() {
     for (let node of nodes) {
+        // check if the user wants to move any node
         if (node.over(mouseX, mouseY)) {
             chosen = node;
             break;
@@ -32,14 +43,17 @@ function mousePressed() {
 }
 
 function mouseDragged() {
+    // let the user move the node around
     if (chosen) {
-        chosen.color = 150;
+        //change the color for the selected node
+        chosen.color = COLOR_CHOSEN;
         chosen.x = mouseX;
         chosen.y = mouseY;
     }
 }
 
 function mouseReleased() {
+    // back to no nodes being selected to move
     chosen.color = COLOR;
     chosen = null;
 }
