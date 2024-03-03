435bee862b35fa2b9d84fbceec32c42cd3205cb3
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-03-02 23:20:07 -0500

    finish interactivity of the nodes

--- sketch.js
+++ sketch.js
@@ -1,47 +1,51 @@
-let node1;
-let node2;
-let edge;
 let COLOR = 255;
+let COLOR_EDGE = 255;
 let WEIGHT = 5;
-let selected;
+let n_nodes = 5;
+let RADIUS = 10;
+
+let nodes = [];
+let chosen = null;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    node1 = new Node(100, 100, 20, COLOR);
-    node2 = new Node(200, 200, 20, COLOR);
+    for (let i = 0; i < n_nodes; i++) {
+        nodes[i] = new Node(random(width), random(height));
+    }
 }
 
 function draw() {
     background(0);
 
-    edge = new Edge(node1, node2, COLOR, WEIGHT);
-    edge.show()
-
-    node1.show();
-    node2.show(); 
-}
-
-function mousePressed() {
-    if (node1.clicked(mouseX, mouseY)) {
-        selected = true;
-    } 
-}
-
-function mouseReleased() {
-    selected = false;
-    node1.color = COLOR;
-}
-
-function mouseDragged() {
-    if (selected) {
-        node1.color = 150;
-        node1.x = mouseX;
-        node1.y = mouseY;
+    for (let i = 0; i < n_nodes; i++) {
+        nodes[i].show();
     }
 }
 
+function mousePressed() {
+    for (let node of nodes) {
+        if (node.over(mouseX, mouseY)) {
+            chosen = node;
+            break;
+        } 
+    }   
+}
+
+function mouseDragged() {
+    if (chosen) {
+        chosen.color = 150;
+        chosen.x = mouseX;
+        chosen.y = mouseY;
+    }
+}
+
+function mouseReleased() {
+    chosen.color = COLOR;
+    chosen = null;
+}
+
 class Node {
-    constructor(_x, _y, _radius, _color) {
+    constructor(_x, _y, _radius = RADIUS, _color = COLOR) {
         this.x = _x;
         this.y = _y;
         this.radius = _radius;
@@ -49,18 +53,20 @@ class Node {
     }
 
     show() {
+        push();
         noStroke();
         fill(this.color);
         circle(this.x, this.y, 2 * this.radius);
+        pop();
     }
 
-    clicked(px, py) {
+    over(px, py) {
         return dist(this.x, this.y, px, py) <= this.radius;
     }
 }
 
 class Edge {
-    constructor(_node1, _node2, _color, _weight) {
+    constructor(_node1, _node2, _color = COLOR_EDGE, _weight = WEIGHT) {
         this.node1 = _node1;
         this.node2 = _node2;
         this.color = _color;
