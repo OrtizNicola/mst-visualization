cf17dab29c9e16c6a1d0b11a9e085800b2380fac
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-03-02 22:48:34 -0500

    add first steps to make the graph interactive

--- sketch.js
+++ sketch.js
@@ -3,34 +3,59 @@ let node2;
 let edge;
 let COLOR = 255;
 let WEIGHT = 5;
+let selected;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(0);
+    node1 = new Node(100, 100, 20, COLOR);
+    node2 = new Node(200, 200, 20, COLOR);
 }
 
 function draw() {
     background(0);
-    node1 = new Node(mouseX, mouseY, 50, COLOR);
-    node1.show();
-    node2 = new Node(200, 200, 50, COLOR);
-    node2.show();
-    
+
     edge = new Edge(node1, node2, COLOR, WEIGHT);
     edge.show()
+
+    node1.show();
+    node2.show(); 
+}
+
+function mousePressed() {
+    if (node1.clicked(mouseX, mouseY)) {
+        selected = true;
+    } 
+}
+
+function mouseReleased() {
+    selected = false;
+    node1.color = COLOR;
+}
+
+function mouseDragged() {
+    if (selected) {
+        node1.color = 150;
+        node1.x = mouseX;
+        node1.y = mouseY;
+    }
 }
 
 class Node {
-    constructor(_x, _y, _size, _color) {
+    constructor(_x, _y, _radius, _color) {
         this.x = _x;
         this.y = _y;
-        this.size = _size;
+        this.radius = _radius;
         this.color = _color;
     }
 
     show() {
+        noStroke();
         fill(this.color);
-        circle(this.x, this.y, this.size);
+        circle(this.x, this.y, 2 * this.radius);
+    }
+
+    clicked(px, py) {
+        return dist(this.x, this.y, px, py) <= this.radius;
     }
 }
 
@@ -43,9 +68,11 @@ class Edge {
     }
 
     show() {
+        push();
         stroke(this.color);
         strokeWeight(this.weight);
         line(this.node1.x, this.node1.y, 
              this.node2.x, this.node2.y); 
+        pop();
     }
 }
